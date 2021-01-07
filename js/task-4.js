450599f133341130f499f4e277b970a4f8228c69
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-01-07 22:03:20 +0200

    add-tasks-3-10

---
+++ js/task-4.js
@@ -0,0 +1,37 @@
+//=-==-=-=-=--=-=-=-=-=-=-=-=-=-===-
+console.log(" ");
+console.log("Task-4");
+console.log(" ");
+//=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
+/*
+
+Задание
+Дополни код, присвоив переменной totalPrice выражение для подсчёта общей суммы заказа. Переменная pricePerItem хранит цену одной единицы товара, а orderedQuantity - количество едниц товара в заказке.
+
+Тесты
+Объявлена переменная pricePerItem.
+Значение переменной pricePerItem это число 3500.
+Объявлена переменная orderedQuantity.
+Значение переменной orderedQuantity это число 4.
+Объявлена переменная totalPrice.
+Значение переменной totalPrice это число 14000.
+Использован оператор *.
+
+*/
+
+//Task START
+
+// Базовый код
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+// Пиши код ниже этой строки
+const totalPrice = pricePerItem * orderedQuantity;
+
+
+//Task END
+
+console.log(" ");
+console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+
