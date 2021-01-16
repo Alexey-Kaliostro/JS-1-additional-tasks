7f37acefd74ae2c0f8d8084d7387c91dece52eb9
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-01-17 00:02:53 +0200

    task-22

--- js/task-22.js
+++ js/task-22.js
@@ -4,13 +4,28 @@ console.log("Task-22");
 console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
+Задание
+Функция checkIfCanAccessContent(subType) проверяет, может ли пользователь получить доступ к контенту. Проверка происходит по типу подписки. Получить доступ могут только пользователи с подпиской pro или vip.
 
+Присвой переменной canAccessContent выражение проверки подписки. Если значение параметра subType равно строкам 'pro' или 'vip', пользователь получит доступ. Результатом выражения проверки будет буль true или false.
+
+Тесты
+Объявлена функция checkIfCanAccessContent(subType).
+В выражении проверки использован оператор ||.
+Вызов checkIfCanAccessContent('pro') возвращает true.
+Вызов checkIfCanAccessContent('starter') возвращает false.
+Вызов checkIfCanAccessContent('vip') возвращает true.
+Вызов checkIfCanAccessContent('free') возвращает false.
 
 */
 
 //Task START
 
+function checkIfCanAccessContent(subType) {
+  const canAccessContent = subType === "pro" || subType === "vip"; // дополни эту строку
 
+  return canAccessContent;
+}
 
 //Task END
 
