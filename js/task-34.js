b07bc62847665d61426387cede5e41c70aba3fe5
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-01-24 23:09:29 +0200

    task-34

--- js/task-34.js
+++ js/task-34.js
@@ -4,12 +4,23 @@ console.log("Task-34");
 console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
+Задание
+Функция normalizeInput(input) принимает строку (параметр input) и возвращает такую же строку, но в нижнем регистре. Присвой переменной normalizedInput выражение создания строки в нижнем регистре из параметра input.
 
+Тесты
+Объявлена функция normalizeInput(input).
+Вызов функции normalizeInput('Привет мир') возвращает 'привет мир'.
+Вызов функции normalizeInput('Это НЕ СпаМ') возвращает 'это не спам'.
+Вызов функции normalizeInput('Большие СКИДКИ') возвращает 'большие скидки'.
 
 */
 
 //Task START
 
+function normalizeInput(input) {
+  const normalizedInput = input.toLowerCase(); // Дополни эту строку
+  return normalizedInput;
+}
 
 
 //Task END
